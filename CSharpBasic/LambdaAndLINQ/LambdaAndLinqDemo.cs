c6ab11efee4f1b9edad0170a5b44cdca1c504ac0
Xuan-Spider <dinhxuan9807@gmail.com>
2024-09-04 00:06:11 +0700

    add LINQ session

--- CSharpBasic/LambdaAndLINQ/LambdaAndLinqDemo.cs
+++ CSharpBasic/LambdaAndLINQ/LambdaAndLinqDemo.cs
@@ -37,13 +37,62 @@ namespace CSharpBasic
 
             Console.WriteLine(square(4)); // Output: 16
 
+
+            // Pass like a param for other function
+            int[] array = new int[4] { 1, 2, 3, 4 };
+
+            Print((x) => x > 2, array);
+
         }
 
 
+        public static void Print(Func<int, bool> func, int[] nums)
+        {
+            foreach (var x in nums)
+            {
+                if(func(x))
+                {
+                    Console.WriteLine(x);
+                }
+            }
+
+        }
+
+
+
+
+        /// <summary>
+        /// LINQ provides a declarative syntax for querying and  manipulating data from various sources, such as collections databases, XML, and more
+        /// </summary>
         public static void LinqDemo()
         {
 
+            int[] numbers = [1, 2, 3, 4];
 
+            // LINQ query syntax
+            var evenNumbers = from number in numbers
+                              where number % 2 == 0
+                              select number;
+
+            // Projection in LINQ
+            var squaredAndFiltered = numbers
+             .Select(x => x * x)
+             .Where(x => x > 10);
+
+
+            // Joining in LINQ
+            var persons = new List<dynamic>();
+            persons.Add(new { Id = 1, Name = "Shawn" });
+            var orders = new List<dynamic>();
+            orders.Add(new { ProductId = 1, PersonId = 1, ProductName = "book" });
+            var query = from person in persons
+                        join order in orders on person.Id equals order.PersonId
+                        select new { person.Name, order.ProductName };  
+
+
+            // LINQ with lambda expressions
+            var products = new List<dynamic> { /* ... */ };
+            var expensiveProducts = products.Where(p => p.Price > 1000);
 
         }
 
